cb4fd377571b6c6ba0125bb6b66efa33ae887d85
MaryShobisha <shobishasanthiyogu@gmail.com>
2023-11-12 19:54:59 +0530

    Basic Angular Project Updated

---
+++ PetCareSystem/src/app/housing-list/housing-list.component.spec.ts
@@ -0,0 +1,20 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HousingListComponent } from './housing-list.component';
+
+describe('AdmiinDashboardComponent', () => {
+  let component: HousingListComponent;
+  let fixture: ComponentFixture<HousingListComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HousingListComponent]
+    });
+    fixture = TestBed.createComponent(HousingListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
